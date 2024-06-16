63bbc1d948258371e7514a8682259c998d0a8c08
x99oly <oliveira.samuel.edu@gmail.com>
2024-06-15 21:17:04 -0300

    card animado / hover - js

--- assets/scripts/scritpt.js
+++ assets/scripts/scritpt.js
@@ -1,10 +1,32 @@
 
 
-
 document.addEventListener("DOMContentLoaded", function () {
 
     let navMenuItems = document.querySelectorAll('.nav-menu');
 
+    let cardList = document.querySelectorAll('.card .card-children');
+
+
+    navHover(navMenuItems);
+    cardHover(cardList);
+
+    
+
+});
+
+function cardHover(cardList){
+    cardList.forEach(function(card) {
+        card.addEventListener("mouseenter", function () {
+            card.style.top = '0';
+        });
+
+        card.addEventListener("mouseleave", function () {
+            card.style.top = '-100%';
+        });
+    });
+}
+
+function navHover(navMenuItems){
     navMenuItems.forEach(function(navMenu) {
         navMenu.addEventListener("mouseenter", function () {
             navMenu.classList.add('on');
@@ -14,7 +36,7 @@ document.addEventListener("DOMContentLoaded", function () {
             navMenu.classList.remove('on');
         });
     });
-});
+}
 
 function disableOnMobile(){
     let wSize = window.innerWidth;
@@ -33,12 +55,14 @@ function disableOnMobile(){
     }
 }
 
-//  CHAMADAS DE FUNÇÕES
-
-disableOnMobile();
 
 //  EVENTLISTNERS 
 
 window.addEventListener('resize', disableOnMobile);
 
+
+
+//  CHAMADAS DE FUNÇÕES
+
+disableOnMobile();
 
