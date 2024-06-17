bd8f95b8e7639777037fd9d3877b7804331070e1
x99oly <oliveira.samuel.edu@gmail.com>
2024-06-17 16:54:14 -0300

    breackpoint
    muito mal organizado

--- assets/scripts/home.js
+++ assets/scripts/home.js
@@ -1,1 +1,62 @@
+// fitAbsolutePos() variables
+let leftS = document.getElementById("left-side");
+let rightS = document.getElementById("right-side");
+let cardInfo = document.querySelector(".card-info")
+
+document.addEventListener("DOMContentLoaded", function () {
+    fitAbsolutePos();
+
+    testeExcluirDepois();
+});
+
+window.addEventListener("resize", function () {
+    fitAbsolutePos();
+
+    testeExcluirDepois();
+})
+
+function testeExcluirDepois(){
+    let a = document.querySelector('absolute-a');
+
+    if(window.innerWidth <= 700){
+        a.classList.remove('absolute-a');
+    }else{
+        a.classList.remove('absolute-a');
+    }
+    
+}
+
+function fitAbsolutePos() {
+
+    if (window.innerWidth <= 700) {
+        leftS.classList.add('d-none');
+        rightS.style.width = '90%';
+        rightS.style.right = '0%';
+        rightS.style.bottom = "15%";
+
+        cardInfo.style.height = '00px';
+        return
+    }
+
+}
+
+
+function changePosition(event) {
+    event.preventDefault();
+
+    let cardChildren = event.currentTarget.querySelector('.card-children');
+
+    if (cardChildren.style.top === '-101%') {
+        cardChildren.style.top = '0';
+    } else {
+        cardChildren.style.top = '-101%';
+    }
+
+}
+
+
+
+
+
+
 
