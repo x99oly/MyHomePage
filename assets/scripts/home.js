77a38f6fa791f3ae568f5a373d019f57e5297e51
x99oly <oliveira.samuel.edu@gmail.com>
2024-06-17 20:18:37 -0300

    index atualizado
    Prontos - estáticos:
    campo main
    campo contato

--- assets/scripts/home.js
+++ assets/scripts/home.js
@@ -1,44 +1,3 @@
-// fitAbsolutePos() variables
-let leftS = document.getElementById("left-side");
-let rightS = document.getElementById("right-side");
-let cardInfo = document.querySelector(".card-info")
-
-document.addEventListener("DOMContentLoaded", function () {
-    fitAbsolutePos();
-
-    testeExcluirDepois();
-});
-
-window.addEventListener("resize", function () {
-    fitAbsolutePos();
-
-    testeExcluirDepois();
-})
-
-function testeExcluirDepois(){
-    let a = document.querySelector('absolute-a');
-
-    if(window.innerWidth <= 700){
-        a.classList.remove('absolute-a');
-    }else{
-        a.classList.remove('absolute-a');
-    }
-    
-}
-
-function fitAbsolutePos() {
-
-    if (window.innerWidth <= 700) {
-        leftS.classList.add('d-none');
-        rightS.style.width = '90%';
-        rightS.style.right = '0%';
-        rightS.style.bottom = "15%";
-
-        cardInfo.style.height = '00px';
-        return
-    }
-
-}
 
 
 function changePosition(event) {
@@ -46,17 +5,59 @@ function changePosition(event) {
 
     let cardChildren = event.currentTarget.querySelector('.card-children');
 
-    if (cardChildren.style.top === '-101%') {
+    if (cardChildren.style.top === '-100%') {
         cardChildren.style.top = '0';
     } else {
-        cardChildren.style.top = '-101%';
+        cardChildren.style.top = '-100%';
     }
-
 }
 
 
+function handleMobileNones(){
 
+    let mls = document.querySelectorAll('.mobile-none');
 
+    if(!returnTrueForMobileWidth()){
+        mls.forEach(ml => {
+            ml.classList.remove('d-none');
+        });
+        return; 
+    }
+    mls.forEach(ml => {
+        ml.classList.add('d-none');
+    })
+}
 
+function page1MobileGridLayout() {
 
+    let content = document.getElementById('content-page1');
 
+    if (returnTrueForMobileWidth()) {
+        content.style.gridTemplateColumns = 'repeat(1, 100%)';
+    } else {
+        content.style.gridTemplateColumns = 'repeat(2, 50%)';
+    }
+}
+
+function returnTrueForMobileWidth() {
+    if (window.innerWidth <= 800) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// CHAMADAS DE MÉTODOS
+
+document.addEventListener("DOMContentLoaded", function () {
+    returnTrueForMobileWidth()
+    page1MobileGridLayout();
+    handleMobileNones();
+});
+
+window.addEventListener("resize", function () {
+    returnTrueForMobileWidth();
+    page1MobileGridLayout();
+    handleMobileNones();
+
+})
