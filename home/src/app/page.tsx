f79d6570851a3155be236cee58654bc4aa0c61af
Sameu Araujo <oliveira.samuel.edu@gmail.com>
2025-02-23 01:22:10 -0300

    sobre mim aparentemente pronto - falta modularizar

--- home/src/app/page.tsx
+++ home/src/app/page.tsx
@@ -11,17 +11,24 @@ export default function Home() {
       <div style={{ height: '8vh', visibility: 'hidden' }}></div> {/* Div fantasma - só quero saltar o espaço*/}
       
       <ContentTwin>
-        <div className='d-grid place-items-centers' style={{ width:'100%', height:'100%'}}>
+        <div className='d-flex justify-content-center align-items-center' style={{ width:'100%', height:'100%'}}>
           <Image
-            src="/X.jpg"  alt="" 
-            width={500}  height={0}
-            style={{ width: '100%', height: 'auto' }}
-          />
+            src="/eu-bg-black.png"  alt="" 
+            width={200}  height={0}
+            style={{
+              width: '40%',
+              height: 'auto',
+            }}
+            />
         </div>
-        <div>
-          <h4>TÍTULO</h4>
-          <h6>SUBTÍTULO</h6>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae dolores ab architecto saepe asperiores perferendis tempora fuga, maxime sit voluptatum beatae placeat dolorem esse magnam assumenda aut possimus autem quidem! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet quibusdam recusandae necessitatibus eum culpa accusamus eveniet expedita quaerat. Quo molestias reprehenderit tempora eum vitae repudiandae sapiente deleniti quia repellat commodi.</p>        
+        <div className='d-flex flex-column justify-content-end align-items-start' style={{width:'100%', height:'100%'}}>
+          <h4>DESENVOLVEDOR FULL-STACK</h4>
+          <h6>C# | .NET | ASP.NET | MySql | Next.js | Python | Flask | Bootstrap</h6>
+          <br></br>
+          <p>
+            Estudante de programação focado em se especializar em desenvolvimento back-end, com ênfase em .NET. Formado como técnico em administração, valorizo a organização e o planejamento como pilares essenciais na construção de projetos. Por isso, busco me constantemente aprimorar meus conhecimentos em arquitetura de software e na aplicação de princípios SOLID.
+          </p>
+        
         </div>  
       </ContentTwin>
     </div>
