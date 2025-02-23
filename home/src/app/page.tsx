5483db56ad7956d031a0822240617dd97f27a4cd
Sameu Araujo <oliveira.samuel.edu@gmail.com>
2025-02-23 00:27:48 -0300

    Reformulada classe ContentTwin - mais simples, mais eficientef

--- home/src/app/page.tsx
+++ home/src/app/page.tsx
@@ -1,47 +1,23 @@
 // src/app/Home.tsx
+import Image from 'next/image';
 import Navbar from '@components/navbar'; 
-import ContentTwin from '@/components/contenttwin'; 
+import ContentTwin from '@/components/contenttwin';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 
 export default function Home() {
   return (
-    <div className="text-white" style={{backgroundColor: 'var(--background)' }}>
-      <Navbar />
-      
-      <div style={{ height: '80px', visibility: 'hidden' }}></div> {/* Div fantasma */}
+    <div className="text-white" style={{ backgroundColor: 'var(--background)' }}>
+      <Navbar />      
+      <div style={{ height: '8vh', visibility: 'hidden' }}></div> {/* Div fantasma - só quero saltar o espaço*/}
       
       <ContentTwin>
-        <div
-          className="d-flex justify-content-end align-items-end"
-          style={{
-            height: '100%',
-            width: '50vw',
-          }}
-        >
-          <div
-            style={{
-              borderTop: '2px solid white',
-              width: '50vw',
-              margin: '20px auto',
-            }}
-          ></div>
+        <div>
+          <Image
+            src="/whale.jpg"  alt="" 
+            width={500}  height={600}
+          />
         </div>
-
-        <div
-          className="d-flex justify-content-start align-items-end w-100"
-          style={{
-            height: '100%',
-            backgroundColor: 'transparent',
-          }}
-        >
-            <div className="d-flex flex-column justify-content-start" style={{ width: '100%' }}>
-              <h3>DESENVOLVEDOR FULL-STACK</h3>
-              <h5>C# | .NET | ASP.NET | MySql | Next.js | Python | Flask | Bootstrap</h5>
-              <p>
-                Estudante de programação focado em se especializar em desenvolvimento back-end, com ênfase em .NET. Formado como técnico em administração, valorizo a organização e o planejamento como pilares essenciais na construção de projetos. Por isso, busco me constantemente aprimorar meus conhecimentos em arquitetura de software e na aplicação de princípios SOLID.
-              </p>
-            </div>
-        </div>
+        <div>Conteúdo 2</div>  
       </ContentTwin>
     </div>
   );
--- home/src/components/contenttwin.tsx
+++ home/src/components/contenttwin.tsx
@@ -2,38 +2,28 @@ import React, { ReactNode } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 interface ContentTwinProps {
-  children: ReactNode[];
+  children: ReactNode[]; // Dois filhos para o conteúdo
 }
 
 const ContentTwin: React.FC<ContentTwinProps> = ({ children }) => {
   return (
-    <div
-      className="d-flex justify-content-between align-items-center flex-wrap"
-      style={{
-        height: '80vh',
-        maxWidth: '80%',
-        margin: '0 auto',
-      }}
-    >
-      <div
-        className="d-flex justify-content-center align-items-center"
-        style={{
-          height: '80%',
-          width: '45%',
-        }}
-      >
-        {children[0]}
-      </div>
-
-      <div
-        className="d-flex justify-content-center align-items-center"
-        style={{
-          height: '80%',
-          width: '45%',
-        }}
-      >
-        {children[1]}
-      </div>
+    <div className="row justify-content-center align-items-center"
+      style={{ height: '90vh', border: '2px solid green', }} >
+        <div className="d-flex h-100 justify-content-center align-items-center"
+          style={{  width: '50vw',  border: '2px solid blue', }}
+        >
+            <div style={{ width: '80%', height: '80%', border: '1px solid yellow', overflow:'hidden' }}>
+              {children[0]}
+            </div>
+        </div>
+        <div
+          className="d-flex h-100 justify-content-center align-items-center"
+          style={{ width: '50vw', border: '2px solid blue', }}
+        >
+          <div style={{ width: '80%', height: '80%', border: '1px solid orange', overflow:'hidden' }}>
+            {children[1]}
+          </div>
+        </div>
     </div>
   );
 };
